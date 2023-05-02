a62fa0e65afa337fb79a71127c79d47d834406f5
“jaemsnrg” <jaemsnrg@gmail.com>
2023-05-02 11:53:22 +0200

    add schema

---
+++ db-schema.md
@@ -0,0 +1,33 @@
+# Table schema - created at https://dbdiagram.io (/d/63bd78856afaa541e5d18a4a)
+
+
+Table orders as O {
+  id bigserial [pk]
+  product varchar [not null, ref: > P.id]
+  customer bigint [not null, ref: > C.id] 
+  quantity bigint [not null]
+  created_at timestamptz [not null, default: `now()`]
+
+  Indexes {
+    product
+    customer
+  }
+}
+
+Table customers as C {
+  id bigserial [pk]
+  first_name varchar [not null]
+  last_name varchar [not null]
+  email varchar [not null]
+  phone varchar [not null]
+  address varchar [not null]
+
+  Indexes {
+    email
+  }
+}
+
+Table products as P {
+  id bigserial [pk]
+  name varchar [not null]
+}
---
+++ opine.sql
@@ -0,0 +1,31 @@
+CREATE TABLE "orders" (
+  "id" bigserial PRIMARY KEY,
+  "product" varchar NOT NULL,
+  "customer" bigint NOT NULL,
+  "quantity" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "customers" (
+  "id" bigserial PRIMARY KEY,
+  "first_name" varchar NOT NULL,
+  "last_name" varchar NOT NULL,
+  "email" varchar NOT NULL,
+  "phone" varchar NOT NULL,
+  "address" varchar NOT NULL
+);
+
+CREATE TABLE "products" (
+  "id" bigserial PRIMARY KEY,
+  "name" varchar NOT NULL
+);
+
+CREATE INDEX ON "orders" ("product");
+
+CREATE INDEX ON "orders" ("customer");
+
+CREATE INDEX ON "customers" ("email");
+
+ALTER TABLE "orders" ADD FOREIGN KEY ("product") REFERENCES "products" ("id");
+
+ALTER TABLE "orders" ADD FOREIGN KEY ("customer") REFERENCES "customers" ("id");
